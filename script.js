ccb032db83ba1468bca1d112bed71fa7f77c2306
ahfasxp <ahfas.fazria@gmail.com>
2024-05-27 15:33:15 +0700

    chore: add connect room

--- script.js
+++ script.js
@@ -36,9 +36,25 @@ document.getElementById("btn").addEventListener("click", function () {
 
   // Update the text in the input field
   function updatecontent(newText) {
-    var canvas = document.querySelector('.kix-canvas-tile-content');
-    var ctx = canvas.getContext('2d');
-    ctx.font = '30px Arial';
+    var canvas = document.querySelector(".kix-canvas-tile-content");
+    var ctx = canvas.getContext("2d");
+    ctx.font = "30px Arial";
     ctx.fillText(newText, 10, 20);
   }
 });
+
+document
+  .getElementById("connect-btn")
+  .addEventListener("click", async function () {
+    const room = new LivekitClient.Room();
+
+    const url = "wss://auto-web-11iwp7z9.livekit.cloud";
+
+    const token = "eyJhbGciOiJIUzI1NiJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6InJvb20tMTg3In0sImlzcyI6IkFQSWJ1ZEpjVHVRVm9zUiIsImV4cCI6MTcxNjgxNDA5MiwibmJmIjowLCJzdWIiOiJ1c2VyLTE4NyJ9.2TtINgdQ-2TrqURqXadOKUJ0zSGmMX_Gsq5NppNeWHE";
+
+    room.prepareConnection(url, token);
+
+    await room.connect(url, token);
+    console.log("connected to room", room.name);
+    alert("Connected to room: " + room.name);
+  });
